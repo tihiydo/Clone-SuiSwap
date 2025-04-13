0d952b544f2b25d0b1bfb28bdee35c2c9821c1c7
adamov.aleksandr.1sp21b <gtivs01@gmail.com>
2025-04-13 19:46:48 +0300

    dd

--- src/components/SwapInterface.tsx
+++ src/components/SwapInterface.tsx
@@ -43,7 +43,7 @@ export default function SwapInterface({ connected }: SwapInterfaceProps) {
                 </Button>
               </div>
               <div className="text-sm text-gray-500 mt-1">
-                $0 USD 
+                $0 USD  
               </div>
             </div>
 
